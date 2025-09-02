297f4a3aa3d16d5f3e1ff177080e4992227aa6e8
kul349 <kkarki349@rku.ac.in>
2025-09-02 19:34:14 +0530

    adding all file

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+
+import React from "react";
+import Logo from "../../assets/logo.png"
+const Navbar=()=>{
+    return (
+        <div>
+      
+
+            <div className="bg-primary/40 p-4 1875rem">
+                <div className="container flex  justify-between items-center">
+<div>
+    <a className="font-bold text-2xl sm:text-3xl flex gap-2"
+     href="#">
+        <img src={Logo} alt="" className="w-10 uppercase" />Shopsy
+    </a>
+</div>
+<div>
+    <div className="group">
+        <input type="text"
+        placeholder="search"
+        className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300  rounded-full border border-gray-300 px-2 py-1 focus:outline-none
+        focus:border-1 focus:border-primary"
+         />
+    </div>
+</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Navbar;
